e74f545188edfbad8ce8f8c9277762cae7db8b38
imo2k <dnrwk99@gmail.com>
2025-02-21 19:34:22 +0900

    Initial Commit

---
+++ SimpeNumberCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpeNumberCounter
+//
+//  Created by GO on 2/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
